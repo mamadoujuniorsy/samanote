6638abf4cc0c699157226e57d907a78a13a1a010
mamadoujuniorsy <mamadoujunior2002@gmail.com>
2025-06-12 12:10:06 +0000

    usage bcrypt au lieu de bcryptjs

--- lib/auth.ts
+++ lib/auth.ts
@@ -3,7 +3,7 @@ import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import GoogleProvider from "next-auth/providers/google"
 import CredentialsProvider from "next-auth/providers/credentials"
 import { prisma } from "./prisma"
-import bcrypt from "bcryptjs"
+import bcrypt from "bcrypt"
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
